b783a4111a06bc96597330c2538dfbab95f85d38
MarcosKern <vkg.marcos@gmail.com>
2025-09-16 23:43:36 +0000

    Att changes

--- main4.cpp
+++ main4.cpp
@@ -2,18 +2,27 @@
 using namespace std;
 
 int main() {
-    bool condicao = true;
-    bool condicao2 = false;
+    int notaA, notaB, notaC;
 
-    if(condicao && condicao2) {
-        cout << "As duas condições são verdadeiras." << endl;
-    }
-    if(condicao || condicao2) {
-        cout << "Pelo menos uma das condições é verdadeira" << endl;
-    }
-    if(!condicao2) {
-        cout << "A segunda condição é falsa" << endl;
-    }
+    cout << "Digite a nota A: ";
+    cin >> notaA;
+    cout << "Digite a nota B: ";
+    cin >> notaB;
+    cout << "Digite a nota C: ";
+    cin >> notaC;
+
+    cout << "As notas digitadas foram: " << notaA << ", " << notaB << ", " << notaC << endl;
+
+    // Exemplos de calculos em C++
+
+    cout << "A nota A + a nota B é: " << notaA + notaB << endl;
+    cout << "A nota A - a nota B é: " << notaA - notaB << endl;
+    cout << "A nota A * a nota B é: " << notaA * notaB << endl;
+    cout << "A nota A / a nota B é: " << notaA / notaB << endl;
+    cout << "A nota A % a nota B é: " << notaA % notaB << endl;
+    cout << "A média das notas é: " << (notaA + notaB + notaC) / 3.0 << endl;
+
+    int notas = notaA + notaB + notaC;
 
     return 0;
 }
---
+++ main5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nome;
+    int notaA, notaB, notaC;
+
+    cout << "Escreva o nome do aluno: ";
+    cin >> nome;
+    cout << "Digite a nota A: ";
+    cin >> notaA;
+    cout << "Digite a nota B: ";
+    cin >> notaB;
+    cout << "Digite a nota C: ";
+    cin >> notaC;
+
+    float media = (notaA + notaB + notaC) / 3.0;
+
+    cout << "=======================================================" << endl;
+    cout << "Aluno: " << nome << endl;
+    cout << "Média: " << media << endl;
+    cout << "Nota A: " << notaA << "; Nota B: " << notaB << "; Nota C: " << notaC << endl;
+    if(media >= 7.0) {
+        cout << "Aprovado!" << endl;
+    } else if(media < 7 && media >= 5) {
+        cout << "Recuperação!" << endl;
+    } else {
+        cout << "Reprovado!" << endl;
+    }
+    cout << "=======================================================" << endl;
+
+
+    return 0;
+}
